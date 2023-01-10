d99f451f7891b58b13ba67aaee4f21539af4187c
fayob <adedejifavour6@gmail.com>
2023-01-10 11:01:59 +0100

    Add optional parameter to item class

--- src/item.rb
+++ src/item.rb
@@ -4,7 +4,7 @@ class Item
   attr_accessor :publish_date, :archived, :label
   attr_reader :id
 
-  def initialize(publish_date, archived)
+  def initialize(publish_date, archived: false)
     @id = rand(100...1000)
     @publish_date = publish_date
     @archived = archived
