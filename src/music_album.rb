18efb70997f04b7763b70fe64a9bc2d43267ed45
fayob <adedejifavour6@gmail.com>
2023-01-10 08:38:56 +0100

    Add music album class with methods

---
+++ src/music_album.rb
@@ -0,0 +1,19 @@
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_reader :name
+
+  def initialize (name, publish_date, archived, on_spotify)
+    super(publish_date, archived)
+    @on_spotify = on_spotify
+    @name = name
+  end
+
+  def can_be_archived?
+    if @archived && @on_spotify
+      true
+    else
+      false
+    end
+  end
+end
