73331f178e1c89b6051e585f01c26ce31b8ed2ec
fayob <adedejifavour6@gmail.com>
2023-01-10 14:37:46 +0100

    Fix linters error

--- src/music_module/spec/genre_spec.rb
+++ src/music_module/spec/genre_spec.rb
@@ -1,13 +1,13 @@
 require_relative '../genre'
 
-describe Genre do 
-  it("should test for instance variable") do
-    genre = Genre.new("John")
-    expect(genre.name).to eql("John")
+describe Genre do
+  it('should test for instance variable') do
+    genre = Genre.new('John')
+    expect(genre.name).to eql('John')
   end
 
-  it("should return an array") do 
-    genre = Genre.new("James")
+  it('should return an array') do
+    genre = Genre.new('James')
     expect(genre.items).to be_a Array
   end
 end
