9d0548dd2a6fbd9b6c92ea2b4b4f41f797a68267
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-10 04:05:39 +0200

    modify ItemReplication class super method call on initiation

--- src/modules/item.rb
+++ src/modules/item.rb
@@ -5,8 +5,8 @@ class ItemReplication < Item
   attr_accessor :publish_date, :archived, :label, :genre, :author
   attr_reader :id
 
-  def initialize(publish_date, label, genre, author, archived: false)
-    super(publish_date, archived)
+  def initialize(publish_date, label, genre, author)
+    super(publish_date)
     @label = label
     @genre = genre
     @author = author
