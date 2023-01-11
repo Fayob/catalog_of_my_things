b70bc02463eb964a81a6997dcc24f57ad616eb66
fayob <adedejifavour6@gmail.com>
2023-01-11 15:44:38 +0100

    Test can_be_archived method in music album

--- src/music_module/music_album.rb
+++ src/music_module/music_album.rb
@@ -11,10 +11,6 @@ class MusicAlbum < Item
   private
 
   def can_be_archived?
-    if @archived && @on_spotify
-      true
-    else
-      false
-    end
+    return true if move_to_archive && @on_spotify
   end
 end
