5868016a7a26908d34cf1b4d243e2aa4889210c9
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-11 17:40:44 +0200

    Add book_label table refrencing both book_id and label_id

--- src/riz/database/schema.sql
+++ src/riz/database/schema.sql
@@ -1,19 +1,3 @@
-CREATE TABLE genre (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE source (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE author (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE label (
   id SERIAL PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
@@ -30,54 +14,13 @@ CREATE TABLE book (
   archived BOOLEAN
   publisher VARCHAR(100),
   cover_state VARCHAR(100),
-  FOREIGN KEY (genre_id) REFERENCES genre (id),
-  FOREIGN KEY (author_id) REFERENCES author (id),
-  FOREIGN KEY (source_id) REFERENCES source (id),
   FOREIGN KEY (label_id) REFERENCES label (id)
 );
 
-CREATE TABLE music_album (
-  id SERIAL PRIMARY KEY,
-  genre_id INT,
-  author_id INT,
-  source_id INT,
-  label_id INT,
-  publisher_date DATE,
-  archived BOOLEAN,
-  on_spotify BOOLEAN,
-  FOREIGN KEY (genre_id) REFERENCES genre (id),
-  FOREIGN KEY (author_id) REFERENCES author (id),
-  FOREIGN KEY (source_id) REFERENCES source (id),
-  FOREIGN KEY (label_id) REFERENCES label (id)
-);
-
-CREATE TABLE movie (
-  id SERIAL PRIMARY KEY,
-  genre_id INT,
-  author_id INT,
-  source_id INT,
-  label_id INT,
-  publisher_date DATE,
-  archived BOOLEAN,
-  silent BOOLEAN,
-  FOREIGN KEY (genre_id) REFERENCES genre (id),
-  FOREIGN KEY (author_id) REFERENCES author (id),
-  FOREIGN KEY (source_id) REFERENCES source (id),
-  FOREIGN KEY (label_id) REFERENCES label (id)
-);
-
-CREATE TABLE game (
-  id SERIAL PRIMARY KEY,
-  genre_id INT,
-  author_id INT,
-  source_id INT,
-  label_id INT,
-  publisher_date DATE,
-  archived BOOLEAN,
-  multiplayer BOOLEAN,
-  last_played_at DATE,
-  FOREIGN KEY (genre_id) REFERENCES genre (id),
-  FOREIGN KEY (author_id) REFERENCES author (id),
-  FOREIGN KEY (source_id) REFERENCES source (id),
-  FOREIGN KEY (label_id) REFERENCES label (id)
-);
+CREATE TABLE book_label (
+  book_id INT NOT NULL,
+  label_id INT NOT NULL,
+  PRIMARY KEY (book_id, label_id),
+  FOREIGN KEY (label_id) REFERENCES label (id),
+  FOREIGN KEY (book_id) REFERENCES book (id)
+)
