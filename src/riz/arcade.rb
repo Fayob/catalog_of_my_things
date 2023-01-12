d2071f0beede3983f5aa8e762f60dd1681438e3c
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-12 12:09:45 +0200

    Add functionality to add label while adding a book and link label with book

--- src/riz/arcade.rb
+++ src/riz/arcade.rb
@@ -7,43 +7,52 @@ class Arcade
   attr_accessor :books, :labels
 
   def initialize
-    @books = fetch_books
     @labels = fetch_labels
+    @books = fetch_books(@labels)
   end
 
+  def add_label
+    print 'Entre Title: '
+    title = gets.chomp
+
+    print 'Entre Color: '
+    color = gets.chomp
+
+    Label.new(title, color)
+  end
+
+  private :add_label
+
+
   def add_book
-    puts 'Enter publish Publish Date:'
+    print 'Enter publish Publish Date: '
     publish_date = gets.chomp.to_s
 
-    puts 'Enter Publisher:'
+    print 'Enter Publisher: '
     publisher = gets.chomp
 
     puts "Enter Cover State:\n(1)Great\n(2)Good\n(3)bad"
     cover_state = gets.chomp.to_i
     cover_state_options = { 1 => 'Great', 2 => 'Good', 3 => 'Bad' }
 
-    @books << Book.new(publish_date, publisher, cover_state_options[cover_state] || 'bad')
+    new_book = Book.new(publish_date, publisher, cover_state_options[cover_state] || 'bad')
+    new_label = add_label
+
+    new_book.modify_label(new_label)
+
+    @books << new_book
+    @labels << new_label
 
     puts 'The book has been added successfully!'
   end
 
-  def add_label
-    puts 'Entre Title:'
-    title = gets.chomp
-
-    puts 'Entre Color'
-    color = gets.chomp
-
-    @labels << Label.new(title, color)
-  end
-
   def add_book_label(_label, _book)
     if @books.legnth && @labels.legnth
-      puts 'Please Select a Book:'
+      print 'Please Select a Book: '
       list_books(true)
       book_i = gets.chomp.to_i
 
-      puts 'Please select a Label:'
+      print 'Please select a Label: '
       list_labels(true)
       label_i = gets.chomp.to_i
 
@@ -58,7 +67,9 @@ class Arcade
   def list_books(with_index)
     @books.each_with_index do |book, i|
       indexing = with_index ? "#{i + 1})" : ''
-      puts "#{indexing} Title: #{book.publisher}, Publisher: #{book.publish_date}, Cover State: #{book.cover_state}"
+      book_data = "Publisher: #{book.publisher}, Publish Date: #{book.publish_date}, Cover State: #{book.cover_state}"
+      label_data = "Title: #{book.label.title}, Color: #{book.label.color}"
+      puts "#{indexing} #{label_data}, #{book_data}"
     end
   end
 
